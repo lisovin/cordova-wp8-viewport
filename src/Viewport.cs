3635cbc4db766e582a9ec966ef510313c9fd6368
Peter Lisovin <peter@lisovin.com>
2014-04-01 14:26:35 -0400

    Initial implementation

---
+++ src/Viewport.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Windows;
+using System.Windows.Media;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using WPCordovaClassLib.Cordova;
+using WPCordovaClassLib.Cordova.Commands;
+using WPCordovaClassLib.Cordova.JSON;
+
+namespace Cordova.Extension.Commands
+{
+    /// <summary>
+    /// </summary>
+    public class Viewport: BaseCommand
+    {       
+        /// <summary>
+        /// </summary>
+        public void getViewport()
+        {
+            DispatchCommandResult(new PlugingResult(PluginResult.Status.OK, "{width:\"480\",height:\"800\"}"));
+        }
+    }
+}
+
---
+++ www/viewport.js
@@ -0,0 +1,10 @@
+﻿var exec = require('cordova/exec');
+
+var ignore = function () {};
+
+module.exports = {
+    getViewport: function (options, onSuccess, onError) {
+        exec(onSuccess || ignore, onError || ignore, "Viewport", "getViewport", [options]);
+    }
+};
+
